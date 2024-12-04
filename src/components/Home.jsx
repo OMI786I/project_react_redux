8f7cf064b52940a98d5f064258f076a6d1f3365d
OMI786I <sohrawardy1998@gmail.com>
2024-12-04 23:03:17 +0600

    added search functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,6 +15,8 @@ const Home = () => {
   const [selectedTask, setSelectedTask] = useState(null);
   const [toggle, setToggle] = useState(false);
 
+  const [search, setSearch] = useState("");
+  console.log(search);
   const task = useSelector((state) => state.tasks);
   console.log(task);
   const [filteredData, setFilteredData] = useState(task);
@@ -51,6 +53,16 @@ const Home = () => {
       })
     );
     console.log(task);
+  };
+
+  const handleSearch = (e) => {
+    const query = e.target.value;
+    setSearch(query);
+
+    const filteredTasks = task.filter((t) =>
+      t.title.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredData(filteredTasks);
   };
 
   const onSubmit = (data) => {
@@ -106,8 +118,31 @@ const Home = () => {
           Add and manage your tasks efficiently.
         </p>
       </header>
+      {task.length != 0 ? (
+        <label className="input input-bordered flex items-center gap-2 my-5">
+          <input
+            type="text"
+            className="grow"
+            placeholder="Search"
+            onChange={handleSearch}
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </label>
+      ) : (
+        ""
+      )}
 
-      {/* Task List */}
       {task.length != 0 ? (
         <div className="flex justify-between">
           <p className="font-bold">Completed</p>
